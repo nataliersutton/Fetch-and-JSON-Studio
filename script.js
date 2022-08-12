aa7e285498e667946a159d81c228c6fa853c6ade
Natalie Sutton <100653980+nataliersutton@users.noreply.github.com>
2022-08-11 21:08:04 -0500

    Added JS code for assignment

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// TODO: add code here
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(function(json) {
+            // console.log(json);
+            const astronautContainer = document.getElementById("container");
+            let astronaut = "";
+            for (astronaut of json) {
+            astronaut += `
+            <div class="astronaut">
+            <div class="bio">
+               <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+               <ul>
+                  <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                  <li>Active: ${astronaut.active}</li>
+                  <li>Skills: ${astronaut.skills.join(", ")}</li>
+               </ul>
+            </div>
+            <img class="avatar" src="${astronaut.picture}"/>
+         </div>
+         `;
+         astronautContainer.innerHTML += astronaut;
+            }
+        });
+    });
+});
